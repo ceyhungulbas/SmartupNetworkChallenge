8217f9f2aac0f440c12acb9a4b31f8f27acd4805
ceyhungulbas <cetree61@gmail.com>
2021-12-02 01:05:19 +0300

    Finalize

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  RefreshControl,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
 
 export default function App() {
+  const [quote, setQuote] = useState({});
+  const [refreshing, setRefreshing] = useState(false);
+
+  async function fetchQuote() {
+    const response = await fetch('https://api.quotable.io/random');
+    const fetchedQuotes = await response.json(response);
+    setQuote(fetchedQuotes);
+  }
+
+  const wait = (timeout) => {
+    return new Promise((resolve) => setTimeout(resolve, timeout));
+  };
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    wait(20).then(() => setRefreshing(false));
+    fetchQuote();
+  }, []);
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+          <View  style={styles.insideView}>
+            <Text>{quote?.content}</Text>
+            <Text>-{quote?.author}</Text>
+            <Text>Tags: {quote?.tags}</Text>
+            <Button onPress={fetchQuote} title="Refresh the quote!" />
+          </View>      
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    display: "flex",
     backgroundColor: '#fff',
+    color: "white",
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
