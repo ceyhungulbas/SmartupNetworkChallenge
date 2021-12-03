5bbfd2085e2257e95e9e24e7c1da9e397c8678e4
ceyhungulbas <cetree61@gmail.com>
2021-12-03 11:55:55 +0300

    Fix: header adjustment

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect, useCallback } from "react";
 import {
-  StyleSheet,
   Text,
   View,
   RefreshControl,
   ScrollView,
   TouchableOpacity,
 } from "react-native";
+import styles from "./StylesheetFromFile";
 
 export default function App() {
   const [quote, setQuote] = useState({
@@ -83,91 +83,3 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  mainDiv: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    alignContent: "stretch",
-    justifyContent: "center",
-    height: "100%",
-  },
-
-  titleMainDiv: {
-    width: "100%",
-    alignItems: "center",
-    justifyContent: "center",
-    height: 45,
-    backgroundColor: "#687089",
-    marginTop: "10%",
-  },
-
-  title: {
-    textAlign: "center",
-    color: "white",
-    width: "100%",
-    fontSize: 20,
-  },
-
-  outsideView: {
-    width: "100%",
-  },
-
-  insideView: {
-    backgroundColor: "#767C96",
-    color: "white",
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: "5%",
-    paddingTop: "25%",
-    paddingBottom: "25%",
-  },
-
-  insideText: {
-    color: "white",
-    fontSize: 18,
-    textAlign: "center",
-  },
-
-  tags: {
-    color: "white",
-  },
-
-  outsideTags: {
-    backgroundColor: "#959AAD",
-    color: "white",
-    marginRight: "3%",
-    marginBottom: "3%",
-    padding: "2%",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 6,
-    },
-    shadowOpacity: 0.37,
-    shadowRadius: 7.49,
-    elevation: 12,
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    textAlign: "center",
-  },
-
-  insideTags: {
-    position: "absolute",
-    right: 0,
-    bottom: 0,
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    flexDirection: "row",
-  },
-
-  buttonStyle: {
-    backgroundColor: "#687089",
-    width: "95%",
-    padding: "3%",
-    textAlign: "center",
-    borderRadius: 15,
-    marginTop: "5%",
-  },
-});
